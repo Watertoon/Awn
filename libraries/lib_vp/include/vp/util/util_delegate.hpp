e359154a2e42efdb96d1404adddb8a84d8eaddbd
watertoon <watertoon4570@gmail.com>
2023-08-21 17:12:15 -0700

    Add a shared interface to the delegate api to allow both Delegate and DelegateFunction to be used interchangeably

--- libraries/lib_vp/include/vp/util/util_delegate.hpp
+++ libraries/lib_vp/include/vp/util/util_delegate.hpp
@@ -2,8 +2,18 @@
 
 namespace vp::util {
 
+    template <class ...Args>
+    class IDelegate {
+        public:
+            constexpr ALWAYS_INLINE  IDelegate() {/*...*/}
+            constexpr ALWAYS_INLINE ~IDelegate() {/*...*/}
+
+            virtual constexpr inline void Invoke(Args... args) = 0;
+            virtual constexpr inline void TryInvoke(Args... args) = 0;
+    };
+
     template <class ParentType, class ...Args>
-    class Delegate {
+    class Delegate : public IDelegate<Args...> {
         public:
             using FunctionType = void (ParentType::*)(Args...);
         public:
@@ -16,19 +26,51 @@ namespace vp::util {
             constexpr ALWAYS_INLINE Delegate(ParentType *parent, FunctionType function) : m_function(function), m_parent(parent) {/*...*/}
             constexpr ~Delegate() {/*...*/}
 
-            constexpr ALWAYS_INLINE void Invoke(Args... args) {
+            virtual constexpr inline void Invoke(Args... args) override {
                 VP_ASSERT(m_parent != nullptr && m_function != nullptr);
                 (m_parent->*m_function)(std::forward<Args>(args) ...);
             }
 
-            constexpr ALWAYS_INLINE void TryInvoke(Args... args) {
+            virtual constexpr inline void TryInvoke(Args... args) override {
                 if (m_parent == nullptr || m_function == nullptr) { return; }
                 (m_parent->*m_function)(std::forward<Args>(args) ...);
             }
     };
 
+    template <class ...Args>
+    class DelegateFunction : public IDelegate<Args...> {
+        public:
+            using FunctionType = void (*)(Args...);
+        public:
+            FunctionType  m_function;
+        public:
+            constexpr ALWAYS_INLINE DelegateFunction() : m_function(nullptr) {/*...*/}
+            constexpr ALWAYS_INLINE DelegateFunction(FunctionType function) : m_function(function) {/*...*/}
+            constexpr ~DelegateFunction() {/*...*/}
+
+            virtual constexpr inline void Invoke(Args... args) override {
+                VP_ASSERT(m_function != nullptr);
+                (*m_function)(std::forward<Args>(args) ...);
+            }
+
+            virtual constexpr inline void TryInvoke(Args... args) override {
+                if (m_function != nullptr) { return; }
+                (*m_function)(std::forward<Args>(args) ...);
+            }
+    };
+
+    template <class ReturnType, class ...Args>
+    class IDelegateReturn {
+        public:
+            constexpr ALWAYS_INLINE  IDelegateReturn() {/*...*/}
+            constexpr ALWAYS_INLINE ~IDelegateReturn() {/*...*/}
+
+            virtual constexpr inline ReturnType Invoke(Args... args) = 0;
+            virtual constexpr inline ReturnType TryInvoke(Args... args) = 0;
+    };
+
     template <class ReturnType, class ParentType, class ...Args>
-    class DelegateReturn {
+    class DelegateReturn : public IDelegateReturn<ReturnType, Args...> {
         public:
             using FunctionType = ReturnType (ParentType::*)(Args...);
         public:
@@ -41,41 +83,19 @@ namespace vp::util {
             constexpr ALWAYS_INLINE DelegateReturn(ParentType *parent, FunctionType function) : m_function(function), m_parent(parent) {/*...*/}
             constexpr ~DelegateReturn() {/*...*/}
 
-            constexpr ALWAYS_INLINE ReturnType Invoke(Args... args) {
+            virtual constexpr inline ReturnType Invoke(Args... args) override {
                 VP_ASSERT(m_parent != nullptr && m_function != nullptr);
                 return (m_parent->*m_function)(std::forward<Args>(args) ...);
             }
 
-            constexpr ALWAYS_INLINE ReturnType TryInvoke(Args... args) {
+            virtual constexpr inline ReturnType TryInvoke(Args... args) override {
                 if (m_parent == nullptr || m_function == nullptr) { return; }
                 return (m_parent->*m_function)(std::forward<Args>(args) ...);
             }
     };
 
-    template <class ...Args>
-    class DelegateFunction {
-        public:
-            using FunctionType = void (*)(Args...);
-        public:
-            FunctionType  m_function;
-        public:
-            constexpr ALWAYS_INLINE DelegateFunction() : m_function(nullptr) {/*...*/}
-            constexpr ALWAYS_INLINE DelegateFunction(FunctionType function) : m_function(function) {/*...*/}
-            constexpr ~DelegateFunction() {/*...*/}
-
-            constexpr ALWAYS_INLINE void Invoke(Args... args) {
-                VP_ASSERT(m_function != nullptr);
-                (*m_function)(std::forward<Args>(args) ...);
-            }
-
-            constexpr ALWAYS_INLINE void TryInvoke(Args... args) {
-                if (m_function != nullptr) { return; }
-                (*m_function)(std::forward<Args>(args) ...);
-            }
-    };
-
     template <class ReturnType, class ...Args>
-    class DelegateReturnFunction {
+    class DelegateReturnFunction : public IDelegateReturn<ReturnType, Args...> {
         public:
             using FunctionType = ReturnType (*)(Args...);
         public:
@@ -85,13 +105,13 @@ namespace vp::util {
             constexpr ALWAYS_INLINE DelegateReturnFunction(FunctionType function) : m_function(function) {/*...*/}
             constexpr ~DelegateReturnFunction() {/*...*/}
 
-            constexpr ALWAYS_INLINE ReturnType Invoke(Args... args) {
+            virtual constexpr inline ReturnType Invoke(Args... args) override {
                 VP_ASSERT(m_function != nullptr);
                 return (*m_function)(std::forward<Args>(args) ...);
             }
 
-            constexpr ALWAYS_INLINE ReturnType TryInvoke(Args... args) {
-                if (m_function != nullptr) { return; }
+            virtual constexpr inline ReturnType TryInvoke(Args... args) override {
+                if (m_function != nullptr) { return 0; }
                 return (*m_function)(std::forward<Args>(args) ...);
             }
     };
